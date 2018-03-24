6d9430ed41e1d7a9370dc4b9f47103c245567780
Hannah Bellamy <hclifton77@gmail.com>
2018-03-24 19:36:36 -0400

    first edit to assignment 2

--- 693 Spring 2018 Assignment 2.R
+++ 693 Spring 2018 Assignment 2.R
@@ -4,7 +4,39 @@ install.packages("nycflights13")
 
 ## load libraries
 library(tidyverse)
+
 library(nycflights13)
 
 ## dataset for assignment
 flights
+
+#other install
+install.packages("dplyr")
+library(dplyr)
+flights <- as_tibble(nycflights13)
+
+#3. flights data: numer of flights canceled per day?
+flights %>% 
+  filter (distance==0)
+
+#4. dep delay
+flights %>% 
+  arrange(desc(dep_delay)) %>%
+  group_by(carrier) 
+
+#4. challenge
+flights %>%
+  group_by(carrier,dest) %>%
+  summarize(n())
+
+#6. which plane has the worst ontime flight record?
+flights %>% 
+  group_by(tailnum) %>% 
+  summarize(meanarr_delay=mean(arr_delay)) %>% 
+  arrange(desc(meanarr_delay))
+#on average, plane N844MH has the worst ontime flight record
+flights %>% 
+  group_by(tailnum) %>% 
+  arrange(desc(arr_delay))
+#flight N384HA has the single worst on time record
+
